8d887520521efaa7b27d267d4c35f87f474c5721
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-03 00:54:57 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,21 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import quoteRoutes from './routes/quote.routes.js'; // Імпортуємо маршрути цитат
-
+import quotesRouter from "./routes/quotes.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+
 dotenv.config();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
-const app = express();
-
 app.use(express.json()); 
 app.use(cookieParser());
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/quotes", quoteRoutes);  
+app.use("/api/quotes", quotesRouter);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
