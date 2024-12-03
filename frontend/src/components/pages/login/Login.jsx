efe740b41863911db364154bd8de345751a9221f
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-04 01:59:51 +0200

    Update Login.jsx

--- frontend/src/components/pages/login/Login.jsx
+++ frontend/src/components/pages/login/Login.jsx
@@ -73,7 +73,7 @@ const Login = () => {
 						className='flex items-center bg-black text-white px-4 py-2 rounded-lg'
 						onClick={() => loginWithSocial("github")}
 					>
-						<img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/GitHub_Logo_2018.svg" alt="GitHub" className="w-5 h-5 mr-2" />
+						<img src="https://img.icons8.com/?size=512&id=52539&format=png" alt="GitHub" className="w-5 h-5 mr-2" />
 						Github
 					</button>
 				</div>
