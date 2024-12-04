e44165a53720b61d48c6ffc9415657abb2c6635d
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-05 01:24:12 +0200

    Update useConversation.js

--- frontend/src/components/zustand/useConversation.js
+++ frontend/src/components/zustand/useConversation.js
@@ -1,10 +1,34 @@
 import { create } from "zustand";
 
-const useConversation = create((set) => ({
-	selectedConversation: null,
-	setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
-	messages: [],
-	setMessages: (messages) => set({ messages }),
+const useConversation = create((set, get) => ({
+  selectedConversation: null,
+  setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
+
+  messages: [],
+  setMessages: (messages) => set({ messages }),
+
+  conversations: [],
+  setConversations: (conversations) => set({ conversations }),
+
+  deleteConversation: (id) => {
+    const { conversations, selectedConversation, setSelectedConversation } = get();
+    const updatedConversations = conversations.filter((conv) => conv.id !== id);
+
+    set({ conversations: updatedConversations });
+
+    if (selectedConversation?.id === id) {
+      setSelectedConversation(null);
+    }
+  },
+
+  editConversation: (id, updatedData) => {
+    const { conversations } = get();
+    const updatedConversations = conversations.map((conv) =>
+      conv.id === id ? { ...conv, ...updatedData } : conv
+    );
+
+    set({ conversations: updatedConversations });
+  },
 }));
 
 export default useConversation;
