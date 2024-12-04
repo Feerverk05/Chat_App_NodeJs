e9dcedc5ad0fdf0517f768df480b8b8def0cd9ad
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-04 02:20:52 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,48 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import session from "express-session"; 
+import passport from "passport";
 import authRoutes from "./routes/auth.routes.js";
+import googleRouter from './controllers/google-auth.js';
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import quotesRouter from "./routes/quotes.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+import facebookRouter from "./controllers/facebook-auth.js"; 
 
 dotenv.config();
-const __dirname = path.resolve();
+
+const __dirname = path.resolve(); 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+app.use("/auth/facebook", facebookRouter);
+app.use('/auth/google', googleRouter);
 app.use("/api/quotes", quotesRouter);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
