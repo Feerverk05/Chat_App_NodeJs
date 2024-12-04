3afe0c86b571195bc7d023f7e8296f01deaf71d5
Feerverk05 <144530169+Feerverk05@users.noreply.github.com>
2024-12-05 01:22:19 +0200

    Update Login.jsx

--- frontend/src/components/pages/login/Login.jsx
+++ frontend/src/components/pages/login/Login.jsx
@@ -16,17 +16,17 @@ const Login = () => {
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Login
+					Увійти
 				</h1>
 
 				<form onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Username</span>
+							<span className='text-base label-text'>Нік</span>
 						</label>
 						<input
 							type='text'
-							placeholder='Enter username'
+							placeholder='Введи нік'
 							className='w-full input input-bordered h-10'
 							value={username}
 							onChange={(e) => setUsername(e.target.value)}
@@ -35,47 +35,51 @@ const Login = () => {
 
 					<div>
 						<label className='label'>
-							<span className='text-base label-text'>Password</span>
+							<span className='text-base label-text'>Пароль</span>
 						</label>
 						<input
 							type='password'
-							placeholder='Enter Password'
+							placeholder='Пароль'
 							className='w-full input input-bordered h-10'
 							value={password}
 							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
 					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
-						{"Don't"} have an account?
+						{"Маєш"} акаунт?
 					</Link>
 
 					<div>
 						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
-							{loading ? <span className='loading loading-spinner'></span> : "Login"}
+							{loading ? <span className='loading loading-spinner'></span> : "Увійти"}
 						</button>
 					</div>
 				</form>
-
-				{/* Соціальні кнопки */}
+				
 				<div className='flex justify-center mt-4'>
+				<button
+  className='flex items-center bg-red-500 text-white px-4 py-2 rounded-lg mr-2'
+  onClick={() => {
+    window.location.href = "http://localhost:5000/auth/google";
+  }}
+>
+  <img src="https://cdn-icons-png.flaticon.com/512/2504/2504914.png" alt="Google" className="w-5 h-5 mr-2" />
+  Google
+</button>
 					<button
-						className='flex items-center bg-red-500 text-white px-4 py-2 rounded-lg mr-2'
-						onClick={() => loginWithSocial("google")}
-					>
-						<img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google" className="w-5 h-5 mr-2" />
-						Google
-					</button>
-					<button onClick={() => {
-   						 window.location.href ="http://localhost:5000/auth/facebook"; }}
-  					className='flex items-center bg-blue-600 text-white px-4 py-2 rounded-lg mr-2'>
-  					<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/800px-Facebook_logo_%28square%29.png" alt="Facebook" className="w-5 h-5 mr-2" /> Facebook </button>
-					<button
-						className='flex items-center bg-black text-white px-4 py-2 rounded-lg'
-						onClick={() => loginWithSocial("github")}
-					>
-						<img src="https://img.icons8.com/?size=512&id=52539&format=png" alt="GitHub" className="w-5 h-5 mr-2" />
-						Github
-					</button>
+  onClick={() => {
+    window.location.href ="http://localhost:5000/auth/facebook/";
+  }}
+  className='flex items-center bg-blue-600 text-white px-4 py-2 rounded-lg mr-2'
+>
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/800px-Facebook_logo_%28square%29.png"
+    alt="Facebook"
+    className="w-5 h-5 mr-2"
+  />
+  Facebook
+</button>
+
 				</div>
 			</div>
 		</div>
